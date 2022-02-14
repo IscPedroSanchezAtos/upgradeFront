8dd1a68b343f9d578f618dd0787433ecd73f1782
IscPedroSanchezatos <pedro-francisco.sanchez@atos.net>
2022-02-14 07:53:59 -0600

    video 9

--- app.js
+++ app.js
@@ -53,11 +53,29 @@ console.log('the parent Element is:', bookList.parentElement.parentElement);
 console.log(bookList.children);*/
 
 // traveling sibling elemets
-const bookList = document.querySelector('#book-list');
+/*const bookList = document.querySelector('#book-list');
 console.log('book-list next sibling is:', bookList.nextSibling);
 console.log('book-list next sibling element is:', bookList.nextElementSibling);
 
 console.log('book-list previous sibling is:', bookList.previousSibling);
 console.log('book-list previous sibling element is:', bookList.previousElementSibling);
 
-bookList.previousElementSibling.querySelector('p').innerHTML += '<br/>Too cool for everyone else';
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br/>Too cool for everyone else';*/
+
+//Events
+var btns = document.querySelectorAll('#book-list .delete');
+
+Array.from(btns).forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        const li = e.target.parentElement;
+
+        li.parentNode.removeChild(li)
+    });
+});
+
+const link = document.querySelector('#page-banner a');
+
+link.addEventListener('click', function(e){
+    e.preventDefault();
+    console.log('navigation to ', e.target.textContent, 'was prevented')
+});
