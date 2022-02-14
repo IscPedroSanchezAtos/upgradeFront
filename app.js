4daae5166cd83c29d44f1fcdda3296015bc5d829
IscPedroSanchezatos <pedro-francisco.sanchez@atos.net>
2022-02-14 07:04:18 -0600

    video 4

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var wrap = document.querySelector('#wrapper');
 //console.log(wrap);
 
 var books = document.querySelector('#book-list li .name');
-//
+//consol.log(books);
 
 var books = document.querySelectorAll('#book-list li .name');
 //console.log(books);
