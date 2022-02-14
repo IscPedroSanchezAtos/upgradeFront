0fca841d1e4a5a26b41cf397f8e34cc6b30b33fc
IscPedroSanchezatos <pedro-francisco.sanchez@atos.net>
2022-02-14 08:15:08 -0600

    video 12

--- app.js
+++ app.js
@@ -90,10 +90,31 @@ list.addEventListener('click',function(e){
 });*/
 
 // Interacting with Forms --- add books
-const addForm = document.forms['add-book'];
+/*const addForm = document.forms['add-book'];
 addForm.addEventListener('submit', function(e){
     e.preventDefault();
     const value = addForm.querySelector('input[type="text"]').value;
     console.log(value);
+});*/
+
+//Creating Elements
+const addForm = document.forms['add-book'];
+addForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+    
+    // create elements
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+    //add content
+    deleteBtn.textContent= 'delete';
+    bookName.textContent= value
+    //append to document
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    FileList.appendChild(li);
+
 });
 
