9bf01b05cad03ddd255ed66dca8010d791bbb41a
IscPedroSanchezatos <pedro-francisco.sanchez@atos.net>
2022-02-14 07:24:58 -0600

    video 6

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ Array.from(books).forEach(function(book){
     book.textContent= 'test';
 });*/
 
-var books= document.querySelectorAll('#book-list li .name');
+/*var books= document.querySelectorAll('#book-list li .name');
 
 Array.from(books).forEach(function(book){
     book.textContent += '(book title)';
@@ -31,4 +31,14 @@ Array.from(books).forEach(function(book){
 
 const bookList = document.querySelector('#book-list');
 bookList.innerHTML = '<h2> Books and more books ..</h2>';
-bookList.innerHTML += '<p> This is how you add HTML </p>';
+bookList.innerHTML += '<p> This is how you add HTML </p>';*/
+
+const banner = document.querySelector('#page-banner');
+
+console.log('#page-banner node type is: ', banner.nodeType);
+console.log('#page-banner node name is: ', banner.nodeName);
+console.log('#page-banner has child nodes:', banner.hasChildNodes);
+
+const cloneBanner = banner.cloneNode(true);
+console.log(cloneBanner);
+
