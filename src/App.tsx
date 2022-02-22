cac89dde289a0f267e5c50e921be9bf18827280c
IscPedroSanchezatos <pedro-francisco.sanchez@atos.net>
2022-02-22 14:41:49 -0600

    useState Hooks with Typescript and passing props to components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputFeild from './components/InputFeild';
 
 
 const App: React.FC = () =>{
+
+  const [todo, setTodo] = useState<string>("");
+console.log(todo);
+
   return (
   <div className="App">
     <span className="heading">Taskify</span>
-    <InputFeild />
+    <InputFeild todo={todo} setTodo={setTodo} />
   </div>
   );
 };
