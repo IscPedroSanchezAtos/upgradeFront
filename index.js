f8ef7788ccfc59621a64ff35c2e409418582ba88
IscPedroSanchezatos <pedro-francisco.sanchez@atos.net>
2022-02-22 00:04:56 -0600

    creted backend api

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-//const { response } = require('express');
-const pa11y = require('pa11y');
+const express = require('express')
+const pa11y = require('pa11y')
+const PORT = process.env.PORT || 5500
 
-async function run() {
-    const response = await pa11y('https://traversy.dev')
-    console.log(response)
-}
+const app = express()
 
-run()
+app.use(express.static('public'))
+
+app.get('/api/test', async (req, res) =>{
+    if(!req.query.url){
+        res.status(400).json({error: 'url is required'})
+    }else {
+        const results = awaitpa11y(req.query.url)
+        res.status(200).json(results)
+    }
+})
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
